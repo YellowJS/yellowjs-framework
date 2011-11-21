66d11065e63ececcca2a28a6fdae152df9f22f61
Mathias Desloges <m.desloges@gmail.com>
2011-11-21 11:26:03 +0100

    add unit test for ooutils

---
+++ tests/core/utils/spec.js
@@ -0,0 +1,23 @@
+describe("ooutils.js", function() {
+
+    describe("scope binding", function () {
+
+        var Obj = {ClsName: 'Cls'};
+
+        var f = function () {
+            return this.ClsName;
+        };
+
+        var f2 = oo.core.utils.bind(f, Obj);
+
+        it('should return a function', function () {    
+            expect(f2).toBeTruthy('function' == typeof f2);
+        });
+
+        it('should return the string \'Cls\'', function () {
+            expect(f2()).toEqual('Cls');
+        });
+
+    });
+
+});
