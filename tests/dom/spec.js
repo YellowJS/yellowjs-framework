367c465af5f222750cfb8381ac8b29b032b3036a
Claire Sosset <cs@octaveoctave.com>
2011-11-22 14:19:18 +0100

    add some unit tests

---
+++ tests/dom/spec.js
@@ -0,0 +1,58 @@
+describe("oodom.js", function() {
+    var ooDom = new oo.View.Dom('#domtest'); 
+    
+    describe("setTranslateX", function () {  
+        it('translateX value should be equal to 100', function () {    
+            ooDom.setTranslateX(100);
+            var matrix = new WebKitCSSMatrix(window.getComputedStyle(ooDom._dom).webkitTransform);
+            expect(matrix.e).toEqual(100);
+        });
+    });
+    
+    describe("setTranslateY", function () {  
+        it('translateY value should be equal to 150', function () {    
+            ooDom.setTranslateY(150);
+            var matrix = new WebKitCSSMatrix(window.getComputedStyle(ooDom._dom).webkitTransform);
+            expect(matrix.f).toEqual(150);
+        });
+    });
+    describe("getTranslateX", function () {  
+        it('value should be equal to matrix.e == 100', function () {    
+            var val = ooDom.getTranslateX(),
+                matrix = new WebKitCSSMatrix(window.getComputedStyle(ooDom._dom).webkitTransform);
+            expect(val).toEqual(matrix.e);
+        });
+    });
+    describe("getTranslateY", function () {  
+        it('value should be equal to matrix.f == 150', function () {    
+            var val = ooDom.getTranslateY(),
+                matrix = new WebKitCSSMatrix(window.getComputedStyle(ooDom._dom).webkitTransform);
+            expect(val).toEqual(matrix.f);
+        });
+    }); 
+    describe("getTranslations", function(){
+        ooDom.setTranslateX(100);
+        ooDom.setTranslateX(150);
+        var val = ooDom.getTranslations(),
+            matrix = new WebKitCSSMatrix(window.getComputedStyle(ooDom._dom).webkitTransform);;
+        
+        it('value should be an array', function () {    
+            expect(val).toBeTruthy(val instanceof Array);
+        });
+        it('first value should be translateX value', function () {    
+            expect(val[0]).toEqual(matrix.e);
+        });
+        it('second value should be translateY value', function () {    
+            expect(val[1]).toEqual(matrix.f);
+        }); 
+    });
+    
+    describe("appendHtml", function(){
+        it('element should should contain <span.test> DomElement', function () {    
+            var txt = '<span class="test"></span>';
+            ooDom.appendHtml(txt);
+            var test = ooDom._dom.querySelector('.test');
+            expect(test).toBeTruthy(jasmine.isDomNode(test));
+        });
+    });
+});
