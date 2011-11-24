b4ac48ccdd4bbd3831dbd34f7c03faf047775ec7
Claire Sosset <cs@octaveoctave.com>
2011-11-24 11:55:07 +0100

    change modele ButtonGroup class to MyClass js modele

--- source/js/view/oobuttongroup.js
+++ source/js/view/oobuttongroup.js
@@ -1,47 +1,47 @@
 var oo = (function (oo) {
 
-    var Dom = oo.Dom, Touch = oo.Touch; Events = oo.Events, Button = oo.Button;
+    var Dom = oo.View.Dom, Touch = oo.Touch, Events = oo.Events, Button = oo.View.Button;
     
-    var ButtonGroup = function ButtonGroup (selector, type) {
-        this._buttons = [];
-        
-        type = type || ButtonGroup.TYPE_RADIO;
-        
-        var buttonList = document.querySelectorAll(selector);
-        
-        var that = this;
-        
-        for (var i=0, len=buttonList.length; i<len; i++) {
-            btn = new Button(buttonList[i]);
-            
-            btn._onRelease = function (e) {
-                _onRelease.call(btn, e, that);
-            };
-            
-            this._buttons.push(btn);
+    var ButtonGroup = my.Class({
+        STATIC : {
+            TYPE_RADIO : 'radio',
+            TYPE_CHECKBOX : 'checkbox'
+        },
+        contructor : function constructor(selector, type) {
+            this._buttons = [];
 
-            if (type == ButtonGroup.TYPE_RADIO) {
-                var that = this;                
-                Events.addListener(Button.EVT_TOUCH, function (triggerBtn, e) {
-                    that.updateActive.apply(that, [triggerBtn, e]);
-                }, btn);                
+            type = type || ButtonGroup.TYPE_RADIO;
+
+            var buttonList = document.querySelectorAll(selector);
+
+            var that = this;
+
+            for (var i=0, len=buttonList.length; i<len; i++) {
+                btn = new Button(buttonList[i]);
+
+                btn._onRelease = function (e) {
+                    _onRelease.call(btn, e, that);
+                };
+
+                this._buttons.push(btn);
+
+                if (type == ButtonGroup.TYPE_RADIO) {
+                    var that = this;                
+                    Events.addListener(Button.EVT_TOUCH, function (triggerBtn, e) {
+                        that.updateActive.apply(that, [triggerBtn, e]);
+                    }, btn);                
+                }
             }
-        }        
-    };
-    
-    ButtonGroup.TYPE_RADIO = 'radio';
-    ButtonGroup.TYPE_CHECKBOX = 'checkbox';
-    
-    var p = ButtonGroup.prototype;
-    
-    p.updateActive = function updateActive (btn, evt) {
-        for (var i=0, len=this._buttons.length; i<len; i++) {
-            if (this._buttons[i] !== btn) {
-                this._buttons[i].setActive(false);
+        },
+        updateActive : function updateActive(btn, evt){
+            for (var i=0, len=this._buttons.length; i<len; i++) {
+                if (this._buttons[i] !== btn) {
+                    this._buttons[i].setActive(false);
+                }
             }
         }
-    };
-    
+    });
+
     // @todo : it's a little bit dirty :s
     // /!\ is called with the scope of the button clicked
     var _onRelease = function _onRelease (e, group) {
@@ -51,7 +51,9 @@ var oo = (function (oo) {
         Events.triggerEvent('release', group, [e, this]);        
     };
     
-    oo.ButtonGroup = ButtonGroup;
-    return oo;
+    var exports = oo.core.utils.getNS('oo.View');
+    exports.ButtonGroup = ButtonGroup;
     
+    return oo;
+
 })(oo || {});
