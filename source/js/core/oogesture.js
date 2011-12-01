1ed7104302340d0011b1ddb75850f4d07114750a
Claire Sosset <cs@octaveoctave.com>
2011-12-01 19:50:11 +0100

    add dispatchEvent but detect gesture fail

--- source/js/core/oogesture.js
+++ source/js/core/oogesture.js
@@ -70,27 +70,25 @@ var oo = (function (oo) {
         startGesture : function startGesture(e){
             //first start : touchFlags.startTime
             this.touchFlags.startTime = Date.now();
-            this.touchFlags.countTouch++;
             
+            //console.log(this.touchFlags.startTime - this.touchFlags.startTimePrevious);
+            /*if ( (this.touchFlags.startTime - this.touchFlags.startTimePrevious) >= 400){
+               this.touchFlags.countTouch = 0; 
+            }*/
+            this.touchFlags.countTouch++;
             
             this.touchFlags.startPosX = this.getXPos(e);
             this.touchFlags.startPosY = this.getYPos(e);
-            
-            console.log(e.touches.length);
-            
-            
-            //alert(this.startPos)
-            //console.log(this.touchFlags.startPosX);
-            //console.log(this.touchFlags.startPosY);
-            
-            //console.log(this.touchFlags.startTime);
+
         },
         moveGesture : function moveGesture(e){
+            if (this.touchFlags.startTime){
+               // console.log('move');
+                this.touchFlags.lastPosX = this.getXPos(e);
+                this.touchFlags.lastPosY = this.getYPos(e);
+                this.touchFlags.hasMoved = true;
+            }
             
-            console.log('move');
-            this.touchFlags.lastPosX = this.getXPos(e);
-            this.touchFlags.lastPosY = this.getYPos(e);
-            this.touchFlags.hasMoved = true;
             
             //console.log(this.touchFlags.lastPosX);
             
@@ -100,26 +98,70 @@ var oo = (function (oo) {
             
             this.touchFlags.stopTime = Date.now();
             
-            console.log("this.touchFlags.lastPosX" + this.touchFlags.lastPosX);
-            console.log("this.touchFlags.lastPosY" + this.touchFlags.lastPosY);
-            console.log(this.touchFlags.countTouch);
+            // console.log("this.touchFlags.lastPosX" + this.touchFlags.lastPosX);
+            //             console.log("this.touchFlags.lastPosY" + this.touchFlags.lastPosY);
+            //             console.log(this.touchFlags.countTouch);
             
             if (this.touchFlags.hasMoved){
                 var deltaX = this.touchFlags.lastPosX - this.touchFlags.startPosX;
                 var deltaY = this.touchFlags.lastPosY - this.touchFlags.startPosY;
                 
                 if ( (Event.HAS_TOUCH && event.targetTouches.length == 1) || !Event.HAS_TOUCH ){
-                    if (Math.abs(deltaX) > 50) {
+                    if (Math.abs(deltaX) > 30) {
                         if ( deltaX > 0 ) {
-                            alert('swipe left');
+                            this.fireEvent(e, "swipeLeft", true, true);
                         } else {
-                            alert('swipe right');
+                            this.fireEvent(e, "swipeRight", true, true);
                         }
                     } 
                 }
                  
-            } else {
-                //test tap or double tap
+            } else { 
+                
+               var that = this;
+               console.log(that.touchFlags) 
+                window.setTimeout(function(e){ 
+                    console.log(that.touchFlags.countTouch);
+                    console.log('testdt '+ (that.touchFlags.stopTime - that.touchFlags.startTimePrevious));
+                    if ( (2 === that.touchFlags.countTouch && (that.touchFlags.stopTime - that.touchFlags.startTimePrevious) < 400)){
+                        console.log("doubletap");
+
+                    } else {  
+                        //console.log('test ' + that.touchFlags.startTime);
+                        if ( (that.touchFlags.stopTime - that.touchFlags.startTime) < 1000 ) {
+                         //this.fireEvent(e, "tap", true, true);
+                         //console.log('tap');
+                         //that.touchFlags.countTouch = 0;
+                         
+                        }
+                        
+                     }
+                    
+                    
+                },200)
+                
+                
+                that.touchFlags.lastPosX = null; 
+                that.touchFlags.lastPosY = null; 
+                that.touchFlags.startPosX = null;
+                that.touchFlags.startPosY = null;
+                that.touchFlags.hasMoved = false;
+                that.touchFlags.startTimePrevious = that.touchFlags.startTime; 
+                that.touchFlags.startTime = null;
+                
+                //test tap or double tap   
+                //alert(this.touchFlags.stopTime - this.touchFlags.startTimePrevious)
+                //if ( (2 === this.touchFlags.countTouch) && ((this.touchFlags.stopTime - this.touchFlags.startTimePrevious) < 200) ){
+                //   alert("doubletap")  
+                //   
+                //} else {
+                //    if ( (this.touchFlags.stopTime - this.touchFlags.startTime) < 200 ) {
+                //        this.fireEvent(e, "tap", true, true);
+                //    }
+                //} 
+                //console.log((this.touchFlags.stopTime - this.touchFlags.startTime) < 150)
+                
+                
                 
             }
             
@@ -138,12 +180,15 @@ var oo = (function (oo) {
             //console.log(this.touchFlags.stopTime);
             
             //reset Pos
-            this.touchFlags.lastPosX = null; 
-            this.touchFlags.lastPosY = null; 
-            this.touchFlags.startPosX = null;
-            this.touchFlags.startPosY = null;
-            this.touchFlags.hasMoved = false;
-            this.touchFlags.startTimePrevious = this.touchFlags.startTime; 
+             
+             
+        },
+        fireEvent : function fireEvent(event, name, bubble, cancelable){
+            //create each Time the event ? 
+            var evt = document.createEvent('Events');  
+            evt.initEvent(name, bubble, cancelable);  
+            //evt.customData = "my custom data"
+            event.target.dispatchEvent(evt);
         }   
     });
     
