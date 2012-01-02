179f0fe05bedb85da4566836d4d5f1cd4a3ddf98
Mathias Desloges <freakdev@gmail.com>
2012-01-02 01:38:54 +0100

    add android-ui demo

---
+++ demos/android-ui/js/app.js
@@ -0,0 +1,46 @@
+var app = (function () {
+    
+    var app = {};
+    //app.stores = {};    
+    
+    window.addEventListener('load', function () {
+        
+        setTimeout( function () {
+            
+            // hide address bar
+            window.scroll(0,0); 
+
+            // prevent page scrolling
+            document.addEventListener('touchmove', function (e) { e.preventDefault(); }, false);
+        
+            app.use_anim = oo.view.Viewport.NO_ANIM;
+        
+            app.viewport = new oo.view.Viewport();
+
+			oo.Router.routes = {
+			    'index': {
+			        url: '/index',
+			        controller: 'index',
+			        action: 'index'
+			    },
+			    'folder': {
+			        url: '/toggle-folder',
+			        controller: 'index',
+			        action: 'HPUContacts'
+			    },
+			    'apps': {
+			        url: '/apps',
+			        controller: 'index',
+			        action: 'apps'
+			    }
+			};
+
+            oo.Router.init();
+            
+        }, 1000);
+        
+    }, false);
+    
+    return app;
+    
+})();
