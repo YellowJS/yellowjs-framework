8021d7f81078e24ec90be1964e5dcb9200bcc8d2
Claire Sosset <cs@octaveoctave.com>
2012-01-17 17:12:52 +0100

    add list test

---
+++ tests/view/list/spec.js
@@ -0,0 +1,21 @@
+describe("oodom.js", function() {
+    var conf = {"model" : "test", "template":"test"};
+    var list = oo.createList(conf);
+    
+    
+    it('list must be an instance of oo.view.List', function(){
+        expect(list instanceof oo.view.List).toBeTruthy();
+    });
+
+    it('_tpl must be equal to "test"', function() {
+        //list.setTemplate('test');
+        expect(list._tpl).toEqual("test");
+    });
+
+    it('_model must be equal to "test"', function() {
+        //list.setModel('test');
+        expect(list._model).toEqual("test");
+    });
+
+
+});
