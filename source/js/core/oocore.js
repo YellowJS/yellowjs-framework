2bbc8e7c0eeef3864225440f13728dc0837beb96
Mathias Desloges <freakdev@gmail.com>
2012-01-18 11:20:30 +0100

    add methode override in core

--- source/js/core/oocore.js
+++ source/js/core/oocore.js
@@ -69,176 +69,25 @@ var oo = (function (oo) {
         EmptyFn: function EmptyFn () { },
         /**
          *
-         *  Secure Hash Algorithm (SHA1)
-         *  http://www.webtoolkit.info/
+         *  returns a unique identifier (by way of Backbone.localStorage.js)
          *
          **/
         generateId: function generateId (tagName) {
-            msg = [tagName, (new Date).getTime(), Math.random().toString()].join('');
-
-            function rotate_left(n,s) {
-                var t4 = ( n<<s ) | (n>>>(32-s));
-                return t4;
-            };
-
-            function lsb_hex(val) {
-                var str="";
-                var i;
-                var vh;
-                var vl;
-
-                for( i=0; i<=6; i+=2 ) {
-                    vh = (val>>>(i*4+4))&0x0f;
-                    vl = (val>>>(i*4))&0x0f;
-                    str += vh.toString(16) + vl.toString(16);
-                }
-                return str;
-            };
-
-            function cvt_hex(val) {
-                var str="";
-                var i;
-                var v;
-
-                for( i=7; i>=0; i-- ) {
-                    v = (val>>>(i*4))&0x0f;
-                    str += v.toString(16);
-                }
-                return str;
-            };
-
-
-            function Utf8Encode(string) {
-                string = string.replace(/\r\n/g,"\n");
-                var utftext = "";
-
-                for (var n = 0; n < string.length; n++) {
-
-                    var c = string.charCodeAt(n);
-
-                    if (c < 128) {
-                        utftext += String.fromCharCode(c);
-                    }
-                    else if((c > 127) && (c < 2048)) {
-                        utftext += String.fromCharCode((c >> 6) | 192);
-                        utftext += String.fromCharCode((c & 63) | 128);
-                    }
-                    else {
-                        utftext += String.fromCharCode((c >> 12) | 224);
-                        utftext += String.fromCharCode(((c >> 6) & 63) | 128);
-                        utftext += String.fromCharCode((c & 63) | 128);
-                    }
-
-                }
-
-                return utftext;
-            };
-
-            var blockstart;
-            var i, j;
-            var W = new Array(80);
-            var H0 = 0x67452301;
-            var H1 = 0xEFCDAB89;
-            var H2 = 0x98BADCFE;
-            var H3 = 0x10325476;
-            var H4 = 0xC3D2E1F0;
-            var A, B, C, D, E;
-            var temp;
-
-            msg = Utf8Encode(msg);
-
-            var msg_len = msg.length;
-
-            var word_array = new Array();
-            for( i=0; i<msg_len-3; i+=4 ) {
-                j = msg.charCodeAt(i)<<24 | msg.charCodeAt(i+1)<<16 |
-                msg.charCodeAt(i+2)<<8 | msg.charCodeAt(i+3);
-                word_array.push( j );
+            var S4 = function () {
+                return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
             }
 
-            switch( msg_len % 4 ) {
-                case 0:
-                    i = 0x080000000;
-                break;
-                case 1:
-                    i = msg.charCodeAt(msg_len-1)<<24 | 0x0800000;
-                break;
+            return ['id-', S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4()].join('');
+        },
 
-                case 2:
-                    i = msg.charCodeAt(msg_len-2)<<24 | msg.charCodeAt(msg_len-1)<<16 | 0x08000;
-                break;
+        /**
+         * mix two object in one, the second override the first one
+         */
+        override: function override (obj, ext) {
+            for (var prop in ext)
+                obj[prop] = ext[prop];
 
-                case 3:
-                    i = msg.charCodeAt(msg_len-3)<<24 | msg.charCodeAt(msg_len-2)<<16 | msg.charCodeAt(msg_len-1)<<8    | 0x80;
-                break;
-            }
-
-            word_array.push( i );
-
-            while( (word_array.length % 16) != 14 ) { word_array.push( 0 ); };
-
-            word_array.push( msg_len>>>29 );
-            word_array.push( (msg_len<<3)&0x0ffffffff );
-
-
-            for ( blockstart=0; blockstart<word_array.length; blockstart+=16 ) {
-
-                for( i=0; i<16; i++ ) { W[i] = word_array[blockstart+i]; };
-                for( i=16; i<=79; i++ ) { W[i] = rotate_left(W[i-3] ^ W[i-8] ^ W[i-14] ^ W[i-16], 1) };
-
-                A = H0;
-                B = H1;
-                C = H2;
-                D = H3;
-                E = H4;
-
-                for( i= 0; i<=19; i++ ) {
-                    temp = (rotate_left(A,5) + ((B&C) | (~B&D)) + E + W[i] + 0x5A827999) & 0x0ffffffff;
-                    E = D;
-                    D = C;
-                    C = rotate_left(B,30);
-                    B = A;
-                    A = temp;
-                }
-
-                for( i=20; i<=39; i++ ) {
-                    temp = (rotate_left(A,5) + (B ^ C ^ D) + E + W[i] + 0x6ED9EBA1) & 0x0ffffffff;
-                    E = D;
-                    D = C;
-                    C = rotate_left(B,30);
-                    B = A;
-                    A = temp;
-                }
-
-                for( i=40; i<=59; i++ ) {
-                    temp = (rotate_left(A,5) + ((B&C) | (B&D) | (C&D)) + E + W[i] + 0x8F1BBCDC) & 0x0ffffffff;
-                    E = D;
-                    D = C;
-                    C = rotate_left(B,30);
-                    B = A;
-                    A = temp;
-                }
-
-                for( i=60; i<=79; i++ ) {
-                    temp = (rotate_left(A,5) + (B ^ C ^ D) + E + W[i] + 0xCA62C1D6) & 0x0ffffffff;
-                    E = D;
-                    D = C;
-                    C = rotate_left(B,30);
-                    B = A;
-                    A = temp;
-                }
-
-                H0 = (H0 + A) & 0x0ffffffff;
-                H1 = (H1 + B) & 0x0ffffffff;
-                H2 = (H2 + C) & 0x0ffffffff;
-                H3 = (H3 + D) & 0x0ffffffff;
-                H4 = (H4 + E) & 0x0ffffffff;
-
-            }
-
-            var temp = cvt_hex(H0) + cvt_hex(H1) + cvt_hex(H2) + cvt_hex(H3) + cvt_hex(H4);
-
-            return ['id-', temp.toLowerCase()].join('');
+            return obj;
         },
 
         /**
@@ -258,7 +107,7 @@ var oo = (function (oo) {
 
         createPanel: function createPanel(panel) {
             if (!(typeof panel == 'object' && 'id' in panel))
-                throw Error('wrong parameter');
+                throw 'Wrong parameter';
 
             var id = panel.id; delete panel.id;
 
--- tests/core/core/spec.js
+++ tests/core/core/spec.js
@@ -1,4 +1,4 @@
-describe("ooutils.js", function() {
+describe("oocore.js", function() {
 
     describe("scope binding", function () {
 
@@ -16,6 +16,34 @@ describe("ooutils.js", function() {
 
         it('should return the string \'Cls\'', function () {
             expect(f2()).toEqual('Cls');
+        });
+
+    });
+
+    describe("override", function () {
+        
+        var baseObj = {
+            'key1':'value1',
+            'key2':'value2'
+        };
+
+        var overObj = {
+            'key1':'new value1',
+            'key3':'new value2'
+        };
+
+        var resultObj = oo.override(baseObj, overObj);
+
+        it("should have \"new value1\" at \"key1\"", function () {
+            expect(resultObj.key1).toEqual("new value1")
+        });
+
+        it("should have \"new value2\" at \"key3\"", function () {
+            expect(resultObj.key3).toEqual("new value2")
+        });
+
+        it("should have \"value2\" at \"key2\"", function () {
+            expect(resultObj.key2).toEqual("value2")
         });
 
     });
