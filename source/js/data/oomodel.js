9362884f29f76acfc1fa813d6f39b0940edfb32d
Claire Sosset <cs@octaveoctave.com>
2012-01-18 15:52:04 +0100

    save method with callback optional

--- source/js/data/oomodel.js
+++ source/js/data/oomodel.js
@@ -7,22 +7,18 @@
     var Model = my.Class(null, oo.core.mixins.Events,{
         constructor: function constructor(options){
             if(!options || (!options.hasOwnProperty('id') || !options.hasOwnProperty('provider')) ) return;
-            
             this._id = options.id;
             this._provider = options.provider;
         },
         fetch : function fetch(callback){
             this._provider.fetch(callback);
         },
-        save : function save(datas){
+        save : function save(datas, callback){
             if(!datas || ( 'object' !== typeof datas )) {
                 throw Error("Data parameter must exist and be an object");
             }
 
-            var callback = function(){};
-
             this._provider.save(datas, callback);
-            
         }
     });
 
