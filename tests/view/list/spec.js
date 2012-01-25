5e67020d58c8b91b67f8424dcd3507ab948cddc2
Claire Sosset <cs@octaveoctave.com>
2012-01-25 11:44:23 +0100

    update test list

--- tests/view/list/spec.js
+++ tests/view/list/spec.js
@@ -11,7 +11,7 @@ describe("oolist.js", function() {
 
     var list2 = oo.createElement('list', {
         model : model,
-        'template' : '<span clss="h1">{{firstname}}</span> | <span clss="h2">{{nickname}}</span>',
+        'template' : '<span class="h1">{{firstname}}</span> | <span class="h2">{{nickname}}</span>',
         'target' : '#target'
     });
 
@@ -25,11 +25,9 @@ describe("oolist.js", function() {
     describe('render', function(){
 
         it("should have a well formated template", function () {
-            expect(list2._tpl).toEqual('<ul>{{#data}}<li class="oo-list-item item-{{key}}"><span clss="h1">{{firstname}}</span> | <span clss="h2">{{nickname}}</span></li>{{/data}}</ul>');
+            expect(list2._tpl).toEqual('<ul>{{#data}}<li data-id="{{key}}" class="oo-list-item"><span class="h1">{{firstname}}</span> | <span class="h2">{{nickname}}</span></li>{{/data}}</ul>');
         });
 
-        model.fetch(function (data) {
-            console.log(data);
-        });
+        model.fetch();
     });
 });
