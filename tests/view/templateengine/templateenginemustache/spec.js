e848e90f399432edb7efbe75e61113d657622d87
Claire Sosset <cs@octaveoctave.com>
2012-01-30 11:01:34 +0100

    fix template engine mustache test

--- tests/view/templateengine/templateenginemustache/spec.js
+++ tests/view/templateengine/templateenginemustache/spec.js
@@ -2,31 +2,37 @@ describe("ootemplateenginemustache.js", function() {
     oo.define({
         templateEngine : 'mustache'
     });
-
+ 
     describe('render',function(){
-      var tpl = '<li>{{title}}</li>';
-      var datas = (new oo.data.Fakeprovider({'name' : "test"}))._datas;
+     var tpl = '{{#datas}}<li>{{nickname}}</li>{{/datas}}';
+      var predatas = (new oo.data.FakeProvider({'name' : "test"}))._data;
+ 
+      var datas = {
+        'datas' : predatas
+      };
+ 
       var templateEngine = new oo.view.templateengine.TemplateEngineMustache();
-      var content = templateEngine.render(datas, tpl);
-
+ 
+      var content = templateEngine.render(tpl, datas);
+ 
       document.querySelector('#target').innerHTML = content;
       var items = document.querySelectorAll('#target li');
-
+ 
       it('content return by render function must be a string', function(){
          expect(typeof content === 'string').toBeTruthy();
       });
-
+ 
       it('list items length must be equal to 2', function(){
          expect(items.length).toEqual(2);
       });
-
+ 
       it('first item content must be value1', function(){
-        expect(items[0].innerHTML).toEqual('value1');
+        expect(items[0].innerHTML).toEqual('Claire_So');
       });
-
+ 
       it('second item content must be value2', function(){
-        expect(items[1].innerHTML).toEqual('value2');
+        expect(items[1].innerHTML).toEqual('FreakDev');
       });
-
+ 
     });
 });
