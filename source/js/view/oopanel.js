87df1d91d28ecec1171557224e6975cdfec86471
Mathias Desloges <freakdev@gmail.com>
2012-01-30 17:02:46 +0100

    cherry pick d778054 - move rendering and display logic into view class

--- source/js/view/oopanel.js
+++ source/js/view/oopanel.js
@@ -6,15 +6,13 @@
  *
  * @author Mathias Desloges <m.desloges@gmail.com> || @freakdev
  */
-var oo = (function (oo) {
+(function () {
 
-    var e = oo.getNS('oo.view'), Dom = oo.view.Dom;
-
-    var Panel = e.Panel = my.Class(Dom, {
+    var Panel =  oo.getNS('oo.view').Panel = my.Class(oo.view.Dom, {
+        // references elements registered into this view
+        _uiElements: {},
         constructor: function constructor() {
-            // references elements registered into this view
-            this._uiElements = {};
-
+            
             Panel.Super.call(this, document.createElement('div'));
 
             if ('init' in this)
@@ -23,22 +21,76 @@ var oo = (function (oo) {
         getEl: function getEl(id) {
             return this._uiElements[id];
         },
-        register: function register(el) {
+        addEl: function addEl(el) {
             this._uiElements[el.getId()] = el;
+        },
+        // deprecated -> please use "addEl()" method instead
+        register: function register(el) {
+            oo.warn('the method oo.view.Panel.register() is deprecated, please use addEl instead');
+            this.addEl(el);
         },
         render: function render() {
             this.classList.addClass('oo-panel');
 
             Panel.Super.prototype.render.call(this);
+            
+            for (var id in this._uiElements) {
+                var el = this._uiElements[id];
+                if (el.needToRender())
+                    el.render();
+            }
         },
-        hide: function hide() {
-            this.setDisplay('none', '');
+        show: function show(direction) {
+            this.setDisplay('block', '');
+            
+            //var index = this._identifierToIndex(panel);
+
+            var Viewport = oo.view.Viewport, vp = oo.getViewport();
+
+            direction = direction || Viewport.ANIM_RTL;
+
+            var anim_duration = 0;
+            if (direction !== Viewport.NO_ANIM) {
+                // prepare transition
+                var translateDist = vp.getWidth() * (direction == Viewport.ANIM_RTL ? 1 : -1);
+                this.setTranslateX(translateDist);
+                // this.getPanel(index).setDisplay('', '');
+                anim_duration = Viewport.ANIM_DURATION;
+            }
+
+            if (!this.panelIsEnable(index)) {
+                this._enablePanel(index);
+            }
+
+            this.getPanel(index).translateTo({x:0}, anim_duration);
+
+            this.triggerEvent('onShow');
         },
-        show: function show() {
-            this.setDisplay('', '');
+        hide: function hide(direction) {
+            //var index = this._identifierToIndex(panel);
+
+            var Viewport = oo.view.Viewport, vp = oo.getViewport();
+
+            direction = direction || Viewport.ANIM_RTL;
+
+            var anim_duration = 0;
+            if (direction !== Viewport.NO_ANIM) {
+                anim_duration = Viewport.ANIM_DURATION;
+            }
+
+            // transition
+            var translateDist = vp.getWidth() * (direction == Viewport.ANIM_RTL ? -1 : 1);
+            // this.getPanel(index).setZIndex(3, '');
+            var that = this;
+            this.translateTo({x:translateDist}, Viewport.ANIM_DURATION, function () {
+                // that.getPanel(index).setDisplay('none');
+                that.setDisplay('none');
+                that.stopAnimation();
+            });
+
+            this.triggerEvent('onHide');
+
         }
     });
 
-    return oo;
-
-})(oo || {});
+})();
