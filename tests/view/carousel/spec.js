a84c9c189ce050a8021054b3736582dbf4eb0e7b
Mathias Desloges <freakdev@gmail.com>
2012-02-02 09:32:05 +0100

    refactor carousel unit test

--- tests/view/carousel/spec.js
+++ tests/view/carousel/spec.js
@@ -1,17 +1,19 @@
 describe("oocarousel.js", function() {
     
+    var provider = new oo.data.FakeProvider({
+        "name" : "fdsfsdf"
+    });
 
-    describe('constructor', function(){
+    var model = oo.createModel({
+        'name' : "test",
+        'provider' : provider
+    });
 
-        var provider = new oo.data.FakeProvider({
-            "name" : "fdsfsdf"
-        });
+    beforeEach(function () {
+        document.querySelector('#carousel').innerHTML = '';
+    });
 
-        var model = oo.createModel({
-            'name' : "test",
-            'provider' : provider
-        });
-
+    describe('model and templates', function () {
         /*test if model and templates exist*/
         /*it('throw error',function(){
             expect(function(){
@@ -53,12 +55,13 @@ describe("oocarousel.js", function() {
                 //expect(carousel._elementCls.test).toEqual(fn);
             
         });*/
+    });
+
+
+    describe('adding content to the carousel', function () {
 
        /* it('elemenCls must be in _elementCls',function(){
             var elementCls = {};
-            
-
-
             
             elementCls.elementA = oo.Class(oo.view.Element, {
                 constructor : function () {
@@ -80,36 +83,35 @@ describe("oocarousel.js", function() {
 
             expect(carousel._elementCls.elementA).toEqual(elementCls.elementA);
         });*/
+    });
 
-        it('showPanel',function(){
-            var elementCls = {};
-            
-            elementCls.elementA = oo.Class(oo.view.Element, {
-                constructor : function () {
-                    elementCls.elementA.Super.call(this, {
-                        target:document.createElement('div'),
-                        template:'<img src="{{picture}}" style=" height:200px; width:100%; display:block;" />'
-                    });
-                },
-                onRendered : function onRendered(){
-                    alert('element A');
-                }
-            });
-
-
-            var optCarousel = {
-                model : model,
-                elementCls : elementCls
-            };
-
-            var carousel = new oo.view.Carousel('#carousel', false, optCarousel);
-
-            expect(function(){
-                carousel.showPanel();
-            }).toThrow("Missing 'id' of the panel");
-            
+    it('showPanel',function(){
+        var elementCls = {};
+        
+        elementCls.elementA = oo.Class(oo.view.Element, {
+            constructor : function () {
+                elementCls.elementA.Super.call(this, {
+                    target:document.createElement('div'),
+                    template:'<img src="{{picture}}" style=" height:200px; width:100%; display:block;" />'
+                });
+            },
+            onRendered : function onRendered(){
+                alert('element A');
+            }
         });
 
+
+        var optCarousel = {
+            model : model,
+            elementCls : elementCls
+        };
+
+        var carousel = new oo.view.Carousel('#carousel', false, optCarousel);
+
+        expect(function(){
+            carousel.showPanel();
+        }).toThrow("Missing 'id' of the panel");
+        
     });
 
 });
