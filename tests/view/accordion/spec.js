276b50b41b58595369c27339a74a2dca3aa23b55
Claire Sosset <cs@octaveoctave.com>
2012-03-05 14:36:42 +0100

    test with multiple accordion

--- tests/view/accordion/spec.js
+++ tests/view/accordion/spec.js
@@ -1,4 +1,9 @@
 describe("oopopin.js", function() {
-    var ac = oo.createElement('accordion', { target:"div#accordion"});
-    
+    var acs = Array.prototype.splice.call(document.querySelectorAll('div.accordion'),0);
+
+    acs.forEach(function(item){
+        var ac = oo.createElement('accordion', { target:item});
+    });
+   // var ac = oo.createElement('accordion', { target:"div.accordion"});
+
 });
