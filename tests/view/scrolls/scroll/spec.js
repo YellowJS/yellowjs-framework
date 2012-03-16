a4dd4d6fb4b25a5976e4c802281ca4dd7adf8ce9
Claire Sosset <cs@octaveoctave.com>
2012-03-16 15:10:26 +0100

    add test for scroll class

---
+++ tests/view/scrolls/scroll/spec.js
@@ -0,0 +1,37 @@
+describe("ooscroll.js", function() {
+    describe("add a second scroll with the same code name", function () {
+        
+        it("should throw an error", function () {
+            expect(function () { oo.view.scroll.Scroll.register(oo.view.scroll.IScroll, 'iscroll'); }).toThrow('Already existing codename');
+        });
+
+    });
+
+    describe("try to get a scroll", function () {
+        
+        it("should return a IScroll", function () {
+            var cls = oo.view.scroll.Scroll.get('iscroll');
+            var obj = new cls();
+            expect(obj instanceof oo.view.scroll.IScroll).toBeTruthy();
+        });
+
+        it("should throw an error", function () {
+            expect(function () { oo.view.scroll.Scroll.get('invalid-scroll'); }).toThrow('Invalid codename');
+        });
+
+    });
+
+    
+    describe("call forbiden method on Scroll \"interface\"", function () {
+
+        var p = new oo.view.scroll.Scroll();
+
+        describe("call scrollTo method", function () {
+        
+            it("should throw an error", function () {
+                expect(function () { p.scrollTo(); }).toThrow();
+            });
+
+        });
+    });
+});
