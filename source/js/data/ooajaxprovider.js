25875d7c7d831d53afbc63ff99d7f5f6ceb69ce9
Mathias Desloges <freakdev@gmail.com>
2012-03-21 22:53:34 +0100

    update oo.data.AjaxProvider to the new oo.override api

--- source/js/data/ooajaxprovider.js
+++ source/js/data/ooajaxprovider.js
@@ -96,6 +96,7 @@
                 params: {}
             };
 
+            config = config || {};
             if (typeof config == 'function') {
                 config = {success: config};
             }
