f41a9029825172d847617fc74529a82916e755a8
Mathias Desloges <freakdev@gmail.com>
2012-04-09 00:13:58 +0200

    quick fix because of API update

--- tests/view/accordion/spec.js
+++ tests/view/accordion/spec.js
@@ -1,8 +1,8 @@
-describe("oopopin.js", function() {
+describe("ooaccordion.js", function() {
     var acs = Array.prototype.splice.call(document.querySelectorAll('div.accordion'),0);
 
     acs.forEach(function(item){
-        var ac = oo.createElement('accordion', { target:item});
+        var ac = oo.createElement('accordion', { el:item});
     });
    // var ac = oo.createElement('accordion', { target:"div.accordion"});
 
