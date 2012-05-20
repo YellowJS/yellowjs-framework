7f2a9c58f97cb7886dc9292c43904f74919f4b3d
Mathias Desloges <freakdev@gmail.com>
2012-05-20 19:34:34 +0200

    add a reference pointing at the Viewport class form the controller

--- source/js/router/oocontroller.js
+++ source/js/router/oocontroller.js
@@ -13,11 +13,14 @@
         _controllers : {},
         // protected
         _viewport: null,
+        _Viewport: null,
         constructor : function constructor(){
             this._viewport = oo.getViewport();
+            this._Viewport = oo.view.Viewport;
         },
         // deprecated - use the class member instead
         getViewport: function getViewport() {
+            console.warn('the getViewport method of the class Controller is deprecated !');
             return oo.getViewport();
         }
     });
