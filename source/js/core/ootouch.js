7faf9c0d3af151bec0a026696d0dcf7b80ccbb4e
Mathias Desloges <freakdev@gmail.com>
2012-05-21 15:21:29 +0200

    merge code from oogesture.js file into ootouch.js

--- source/js/core/ootouch.js
+++ source/js/core/ootouch.js
@@ -5,6 +5,8 @@
  * @class Touch
  *
  * @author Mathias Desloges <m.desloges@gmail.com> || @freakdev
+ * @author Claire Sosset <claire.sosset@gmail.com> || @claire_so
+ *
  */
 (function (oo) {
 
@@ -92,16 +94,113 @@
             HAS_TOUCH : 'ontouchstart' in window ? true : false
         }
     });
-     
-    if (!Touch.HAS_TOUCH) {
-        Touch.EVENT_START = 'mousedown';
-        Touch.EVENT_MOVE  = 'mousemove';
-        Touch.EVENT_END   = 'mouseup';
-    } else {
-        Touch.EVENT_START = 'touchstart';
-        Touch.EVENT_MOVE  = 'touchmove';
-        Touch.EVENT_END   = 'touchend';
-    }
+
+
+
+    // gesture management
+
+    (function () {
+
+
+        var touchFlags = {
+            el : null, startTime : null, stopTime : null, hasMoved : false, startX : null, startY : null, lastX : null, lastY : null, time : 150, timeout : null, doubleTap : false
+        };
+
+        var startGesture = function startGesture(e){
+            touchFlags.el = e.target;
+            touchFlags.startTime = Date.now();
+            touchFlags.hasMoved = false;
+            touchFlags.startX = Touch.getPositionX(e);
+            touchFlags.startY = Touch.getPositionY(e);
+ 
+            if ( (touchFlags.startTime -touchFlags.stopTime) < touchFlags.time) {
+                touchFlags.doubleTap = true;
+                window.clearTimeout(touchFlags.timeout);
+            } else {
+                touchFlags.doubleTap = false;
+            }
+        };
+
+        var moveGesture = function moveGesture(e){
+            //condition cause mousemove
+            if (touchFlags.startTime){
+                touchFlags.lastX = Touch.getPositionX(e);
+                touchFlags.lastY = Touch.getPositionY(e);
+                touchFlags.hasMoved = true;
+            }
+        };
+
+        var stopGesture = function stopGesture(e){
+
+            touchFlags.stopTime = Date.now();
+
+            if (touchFlags.hasMoved){
+                var deltaX = touchFlags.lastX - touchFlags.startX,
+                    deltaY = touchFlags.lastY - touchFlags.startY;
+
+                  // removed because it is useless and breaks the gesture detection
+//                if ( (Touch.HAS_TOUCH && e.targetTouches.length == 1) || !Touch.HAS_TOUCH ){
+                    if (Math.abs(deltaX) > 30 && Math.abs(deltaY) < 100 ) {
+                        if ( deltaX < 0 ) {
+                            fireEvent(touchFlags.el, "swipeLeft", true, true);
+                        } else {
+                            fireEvent(touchFlags.el, "swipeRight", true, true);
+                        }
+                    }
+//                }
+            } else {
+               //https://github.com/madrobby/zepto/blob/master/src/touch.js
+               touchFlags.timeout = window.setTimeout(function(){
+                   touchFlags.timeout = null;
+
+                   if ( touchFlags.doubleTap){
+                       fireEvent(touchFlags.el, "doubleTap", true, true);
+                   } else {
+                       fireEvent(touchFlags.el, "tap", true, true);
+                   }
+               }, touchFlags.time);
+    
+            }
+            
+            touchFlags.lastX = touchFlags.lastY = touchFlags.startX = touchFlags.startY = null;
+
+        };
+
+        var fireEvent = function fireEvent(target, name, bubble, cancelable) {
+            //create each Time the event ?
+            var evt = document.createEvent('Events');
+            evt.initEvent(name, bubble, cancelable);
+            //evt.customData = "my custom data"
+            target.dispatchEvent(evt);
+        };
+
+
+        if (!Touch.HAS_TOUCH) {
+            Touch.EVENT_START = 'mousedown';
+            Touch.EVENT_MOVE  = 'mousemove';
+            Touch.EVENT_END   = 'mouseup';
+        } else {
+            Touch.EVENT_START = 'touchstart';
+            Touch.EVENT_MOVE  = 'touchmove';
+            Touch.EVENT_END   = 'touchend';
+        }
+
+        //attach events to document
+        document.addEventListener(Touch.EVENT_START, function(e){
+            startGesture(e);
+        }, false);
+        
+        document.addEventListener(Touch.EVENT_MOVE, function(e){
+            e.preventDefault();
+            moveGesture(e);
+        }, false);
+        
+        document.addEventListener(Touch.EVENT_END, function(e){
+            stopGesture(e);
+        }, false);
+
+    })();
+
      
     var exports = oo.getNS('oo.core');
     exports.Touch = Touch;
