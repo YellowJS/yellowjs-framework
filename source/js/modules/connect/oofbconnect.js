398bfe5eb7b126631be44dde2f9f6b717a4a43f5
Mathias Desloges <freakdev@gmail.com>
2012-08-17 12:13:10 +0200

    add _ensureInit metod and wrap FB API call to avoid call when the API is not initialized

--- source/js/modules/connect/oofbconnect.js
+++ source/js/modules/connect/oofbconnect.js
@@ -8,8 +8,31 @@
     var FBConnect = oo.getNS('oo.modules.connect').FBConnect = oo.Class(Connect, {
         constructor : function constructor(opts) {
             FBConnect.Super.call(this,opts);
+            var self = this;
+
+            FBConnect.Super.call(this,opts);
+
+            this._fbIsInit = false;
+
             //init the facebook api
             FB.init(opts);
+
+
+            CDV.FB.getLoginStatus(function () {
+                self._fbIsInit = true;
+            });
+        },
+        _ensureInit: function _ensureInit (callback) {
+            var self = this;
+            if(!this._fbIsInit) {
+                setTimeout(function() {self._ensureInit(callback);}, 50);
+            } else {
+                if(callback) {
+                    callback();
+                } else {
+                    return true;
+                }
+            }
         },
         login: function login(callback,opts){
             if(!callback){
@@ -18,33 +41,40 @@
             if(!opts){
                 opts = {};
             }
-
-            FB.login(function(response){
-                callback(response);
-            },opts);
+            this._ensureInit(function () {
+                FB.login(function(response){
+                    callback(response);
+                },opts);
+            });
         },
         logout:function logout(callback){
             if(!callback){
                 callback = oo.emptyFn;
             }
-            FB.logout(function(response){
-                callback(response);
+            this._ensureInit(function () {
+                FB.logout(function(response){
+                    callback(response);
+                });
             });
         },
         getLoginStatus:function getLoginStatus(callback){
             if(!callback){
                 callback = oo.emptyFn;
             }
-            FB.getLoginStatus(function(response){
-                callback(response);
+            this._ensureInit(function () {
+                FB.getLoginStatus(function(response){
+                    callback(response);
+                });
             });
         },
         getUser: function getUser(callback){
             if(!callback){
                 callback = oo.emptyFn;
             }
-            FB.api('/me', function(response) {
-                callback(response);
+            this._ensureInit(function () {
+                FB.api('/me', function(response) {
+                    callback(response);
+                });
             });
         }
     });
